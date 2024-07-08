9064949f1f0c6d81fa176cf572851821af693404
babyo77 <onlybabyyo7@gmail.com>
2024-07-08 11:17:06 +0530

    initial

--- server.js
+++ server.js
@@ -31,9 +31,7 @@ app.get("/:u", async (req, res) => {
     let page = await browser.newPage();
 
     // await page.setViewport({ width: 1366, height: 768 });
-    await page.setUserAgent(
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-    );
+
     await page.goto(`https://www.instagram.com/${u}`);
     await page.waitForSelector("header");
 
